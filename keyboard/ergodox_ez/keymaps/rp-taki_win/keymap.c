d22d90bbd3b88d2d7d10d38a266c8eb81f9cd14d
rp.taki <rp.taki@gmail.com>
2016-06-20 08:25:17 -0700

    commit

--- keyboard/ergodox_ez/keymaps/rp-taki_win/keymap.c
+++ keyboard/ergodox_ez/keymaps/rp-taki_win/keymap.c
@@ -12,19 +12,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | Grv    |   1  |   2  |   3  |   4  |   5  |[Esc] |           |PrtSc |   6  |   7  |   8  |   9  |   0  | BkSpc  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |[+L1] |           |  -   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  =   |           |  -   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |   '    |
- * |--------+------+------+------+------+------| Grv  |           |  =   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |LCtrl | LAlt | LGUI |   -  |[~L1] |                                       | Left | Down |  Up  |Right |[~L1] |
+ *   |LCtrl | LAlt | LGUI | LAlt |[~L1] |                                       | Left | Down |  Up  |Right |[~L1] |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |[Esc] |[~L1] |       |  [   |   ]  |
+ *                                        |[Esc] |[+L1] |       |  [   |   ]  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space| LAlt |------|       |------| BkSpc  |Enter |
+ *                                 | Space| BkSpc|------|       |------| [~L1]  |Enter |
  *                                 |      |      | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -33,22 +33,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_GRV,  KC_1,     KC_2,    KC_3,    KC_4,   KC_5,    KC_ESC,
-        KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,    TG(SYMB),
+        KC_TAB,  KC_Q,     KC_W,    KC_E,    KC_R,   KC_T,    KC_EQL,
         KC_LCTRL,KC_A,     KC_S,    KC_D,    KC_F,   KC_G,
-        KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,    KC_GRV,
-        KC_LCTRL, KC_LALT, KC_LGUI, KC_MINS, MO(SYMB),
-                                             KC_ESC, MO(SYMB),
+        KC_LSFT, KC_Z,     KC_X,    KC_C,    KC_V,   KC_B,    KC_LBRC,
+        KC_LCTRL, KC_LALT, KC_LGUI, KC_LALT, MO(SYMB),
+                                             KC_ESC, TG(SYMB),
                                                               KC_HOME,
-                                             KC_SPC, KC_LALT, KC_END,
+                                             KC_SPC, KC_BSPC, KC_END,
         // right hand
              KC_PSCR, KC_6,    KC_7,    KC_8,    KC_9,   KC_0,     KC_BSPC,
              KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,     KC_BSLS,
                       KC_H,    KC_J,    KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
-             KC_EQL,  KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
+             KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH,  KC_RSFT,
                                KC_LEFT, KC_DOWN, KC_UP,  KC_RIGHT, MO(SYMB),
              KC_LBRC, KC_RBRC,
              KC_PGUP,
-             KC_PGDN, KC_BSPC, KC_ENT
+             KC_PGDN, MO(SYMB),KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
