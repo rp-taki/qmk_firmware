1845ef9844eec2d03dd3eb070c088a96cf84d58c
Takashi Sato <bizes.taki@gmail.com>
2019-12-09 00:33:59 +0900

    reset and swap opt,cmd

--- keyboards/boardwalk/keymaps/rp-taki/keymap.c
+++ keyboards/boardwalk/keymaps/rp-taki/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* 1uGrid
  * .-----------------------------------------------------------------------------------------------------------------------------.
- * | ESC    | 1      | 2      | 3      | 4      | 5      | DEL    | ~      | 6      | 7      | 8      | 9      | 0      | |      |
+ * | ESC    | 1      | 2      | 3      | 4      | 5      | RESET  | ~      | 6      | 7      | 8      | 9      | 0      | |      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
  * | TAB    | Q      | W      | E      | R      | T      | -      | =      | Y      | U      | I      | O      | P      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
@@ -37,16 +37,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | (      | )      | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+-----------------+--------+--------|
- * | LGUI   | LALT   | LGUI   | FN     | BACKSP | SPACE  | SPACE  | ENTER  | ENTER  | FN     | LEFT   | DOWN   | UP     | RIGHT  |
+ * | LALT   | LGUI   | LALT   | FN     | BACKSP | SPACE  | SPACE  | ENTER  | ENTER  | FN     | LEFT   | DOWN   | UP     | RIGHT  |
  * '-----------------------------------------------------------------------------------------------------------------------------'
  */
 
  [_1U] = LAYOUT_ortho_5x14(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_DEL,  KC_GRV,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
+  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    RESET,  KC_GRV,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS, KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC, KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN, KC_RPRN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
-  KC_LGUI, KC_LALT, KC_LGUI, MO(1),   KC_BSPC, KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LALT, KC_LGUI, KC_LALT, MO(1),   KC_BSPC, KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
  ),
 
  /* HHKB
