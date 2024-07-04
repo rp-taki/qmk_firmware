341b802cbbfa3b6d3867bd53b368e8edd51153bf
Takashi Sato <sato@mamezou.com>
2024-07-04 16:53:41 +0900

    -+

--- keyboards/boardwalk/keymaps/rp-taki/keymap.c
+++ keyboards/boardwalk/keymaps/rp-taki/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * .-----------------------------------------------------------------------------------------------------------------------------.
  * | ESC    | 1      | 2      | 3      | 4      | 5      | RESET  | ~      | 6      | 7      | 8      | 9      | 0      | |      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------|
- * | TAB    | Q      | W      | E      | R      | T      | =      | -      | Y      | U      | I      | O      | P      | BACKSP |
+ * | TAB    | Q      | W      | E      | R      | T      | -      | =      | Y      | U      | I      | O      | P      | BACKSP |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+-----------------+--------|
  * | LCTRL  | A      | S      | D      | F      | G      | [      | ]      | H      | J      | K      | L      | ;      | '      |
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
  [_1U] = LAYOUT_ortho_5x14(
   KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    RESET,   KC_GRV,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, \
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_EQL,  KC_MINS, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS, KC_EQL,  KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC, KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_LPRN, KC_RPRN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, \
   KC_LGUI, KC_LALT, KC_LGUI, KC_LALT, KC_BSPC, KC_SPC,  KC_SPC,  KC_ENT,  KC_ENT,  MO(1),   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
